fe663949e3044aea839867ffc3c6bf442f13d7e8
Shivam <shivamchoudhary1824@gmail.com>
2026-09-02 10:13:15 +0530

    Initial Book Management API setup

---
+++ src/main/java/com/practice/intern/Book/Management/API/demo/BookManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.practice.intern.Book.Management.API.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/intern/Book/Management/API/demo/BookManagementApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.intern.Book.Management.API.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
