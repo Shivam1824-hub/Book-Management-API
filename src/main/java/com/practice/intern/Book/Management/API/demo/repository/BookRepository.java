2c4322c0658f0b613ef137aee266bfe19be4b57d
Shivam <shivamchoudhary1824@gmail.com>
2026-09-06 12:28:32 +0530

    implement find by title and author in Repository

--- src/main/java/com/practice/intern/Book/Management/API/demo/repository/BookRepository.java
+++ src/main/java/com/practice/intern/Book/Management/API/demo/repository/BookRepository.java
@@ -4,7 +4,9 @@ import com.practice.intern.Book.Management.API.demo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    public  List<Book> findByTitleContainsIgnoreCaseOrAuthorContainsIgnoreCase(String titleKeyword,String authorKeyword);
 }
