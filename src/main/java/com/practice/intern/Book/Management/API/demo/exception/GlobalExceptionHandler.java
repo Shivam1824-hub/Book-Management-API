524c5cd49a524a0c7dbd419c689fa897f7c09829
Shivam <shivamchoudhary1824@gmail.com>
2026-09-11 10:55:10 +0530

    implement DTO's exception handler validation

--- src/main/java/com/practice/intern/Book/Management/API/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/practice/intern/Book/Management/API/demo/exception/GlobalExceptionHandler.java
@@ -3,8 +3,14 @@ package com.practice.intern.Book.Management.API.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +18,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<String> handleBookNotFound(BookNotFoundException ex){
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName =((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
 }
